1f967adb6e23a014a4468847ed734dffda3bd51c
twangyal <tsering.w1029@gmail.com>
2026-08-13 13:25:04 -0400

    Remove visual-truth dependency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import { AccountBar } from "@/src/app/AccountBar";
 import { Sidebar } from "./Sidebar";
-import { VisualTruthDev } from "./VisualTruthDev";
 
 export default function RootLayout({
   children,
@@ -11,7 +10,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="h-screen w-screen flex flex-col overflow-hidden bg-white">
-        {process.env.NODE_ENV !== "production" ? <VisualTruthDev /> : null}
         <main className="flex-1 overflow-hidden w-full">
         <div className="h-full flex w-full bg-white">
           <Sidebar/>
